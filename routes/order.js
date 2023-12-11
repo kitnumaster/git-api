326c8d20b010181be95c1ed6dd416f151f096e5d
Kitsanu Arsiraphat <kitsanuarsiraphat@GTXTech004s-MacBook-Pro.local>
2023-12-11 23:06:34 +0700

    go-to-payment-card

--- routes/order.js
+++ routes/order.js
@@ -34,4 +34,43 @@ router.post(
     CreditCardPayment
 )
 
+router.post(
+    '/go-to-payment-card',
+    (req, res, next) => {
+
+        const axios = require("axios")
+        let data = req.body
+        const { order } = data;
+        const body = {
+            "access_key": process.env.MERCHANT_ACCESS_KEY || "bb47bf1d20833eaba29a5477ce518946",
+            "profile_id": process.env.MERCHANT_PROFILE_ID || "EF16D9E3-DAC3-4F89-88B7-1D31BFDDA944",
+            "transaction_uuid": generatePassword(),
+            "signed_field_names": "access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency",
+            "unsigned_field_names": "",
+            "signed_date_time": new Date().toISOString(),
+            "locale": "en",
+            "transaction_type": "sale",
+            "reference_number": order.orderNumber,
+            "amount": order.totalPrice,
+            "currency": "THB",
+            // "card_type" : "001",
+            // "card_number" : "4242424242424242",
+            // "card_expiry_date" : "12-2030",
+        }
+
+        console.log("gotoPaymentCredit body", body);
+
+        axios.post(process.env.CYBERPAY_BASE_URL || "https://testsecureacceptance.cybersource.com/pay", body, {}).then((res) => {
+            console.log("gotoPaymentCredit res", res);
+            if (res.data) {
+                // window.location.href = res.data.redirect_url;
+                res.redirect(res.data.redirect_url);
+            }
+        }).catch((err) => {
+            showNotify(err.message, "error");
+            closeModalPolicy.click();
+            console.log("gotoPaymentCredit err", err);
+        })
+    })
+
 module.exports = router
