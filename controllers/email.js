2244054580d41b9e9c5637c9e467e447a3947605
olive <lactoyen@gmail.com>
2023-12-11 17:59:02 +0700

    เพิ่มภาษาอีเมล

--- controllers/email.js
+++ controllers/email.js
@@ -78,8 +78,9 @@ const NewUser = async (to, userName, activateCode) => {
     let subject = "[GIT Jewely Design Gallery] New User Registration"
 
     let userFullName = userName
-    let activateUrl = 'http://58.181.138.201:3004/account/activate/' + activateCode;
-    let msg = "Welcome to GIT Jewely Design Gallery, " + userFullName + "." + "<br>" + "Please click the link below to activate your account." + "<br>" + "<br>" + "New user link activate : <a href='" + activateUrl + "'>" + activateUrl + "</a><br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
+    let activateUrl = 'http://designgallery.git.or.th:3004/account/activate/' + activateCode;
+    let msg = "ยินดีต้อนรับสู่ GIT Jewely Design Gallery, " + userFullName + "." + "<br>" + "กรุณาคลิกลิ้งด้านล่างเพื่อ activate บัญชีของคุณ" + "<br>" + "<br>" + "ลิ้งค์สำหรับ activate บัญชี : <a href='" + activateUrl + "'>" + activateUrl + "</a><br>" + "<br>" + "ขอบคุณค่ะ," + "<br>" + "GIT Jewely Design Gallery Team <br><br>"
+        msg +="Welcome to GIT Jewely Design Gallery, " + userFullName + "." + "<br>" + "Please click the link below to activate your account." + "<br>" + "<br>" + "New user link activate : <a href='" + activateUrl + "'>" + activateUrl + "</a><br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
 
     SendEmail(subject, msg, to, adminEmail)
 }
@@ -89,7 +90,8 @@ const RegisterDesigner = async (to, userFullName = "") => {
     let adminEmail = await getMailAdmin()
     let subject = "[GIT Jewely Design Gallery] Designer Registration"
     // let userFullName = userName
-    let msg = "Hello, " + userFullName + "<br>" + "<br>" + "You have been registered as a designer in GIT Jewely Design Gallery." + "<br>" + "Please wait for the administrator to approve your registration." + "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
+    let msg = "สวัสดี, คุณ" + userFullName + "<br>" + "<br>" + "คุณได้สมัครสมาชิกเป็นนักออกแบบ ของ GIT Jewely Design Gallery." + "<br>" + "กรุณารอการติดต่อกลับของแอดมิน เพื่ออนุมัติการสมัครสมาชิคของท่าน" + "<br>" + "<br>" + "ขอบคุณค่ะ," + "<br>" + "GIT Jewely Design Gallery Team <br><br>"
+        msg += "Hello, " + userFullName + "<br>" + "<br>" + "You have been registered as a designer in GIT Jewely Design Gallery." + "<br>" + "Please wait for the administrator to approve your registration." + "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
 
     // console.log(adminEmail)
 
@@ -101,7 +103,8 @@ const ApproveDesigner = async (to, userFullName = "") => {
 
     let subject = "[GIT Jewely Design Gallery] Approve Designer"
     // let userFullName = "";
-    let msg = "Congratulations, " + userFullName + "<br>" + "<br>" + "you have been approved as a designer in GIT Jewely Design Gallery." + "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
+    let msg = "ขอแสดงความยินดีกับ, คุณ" + userFullName + "<br>" + "<br>" + "คุณได้รับการอนุมัติเป็นนักออกแบบ ของ GIT Jewely Design Gallery." + "<br>" + "<br>" + "ขอบคุณค่ะ," + "<br>" + "GIT Jewely Design Gallery Team <br><br>"
+        msg += "Congratulations, " + userFullName + "<br>" + "<br>" + "you have been approved as a designer in GIT Jewely Design Gallery." + "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
 
     // console.log(adminEmail)
 
@@ -122,10 +125,18 @@ const NewOrder = async (to, userFullName, orderId, orderNumber, orderCreateDate,
     // paymentAmount = "";
     let PaymentStatus = 1;
     let paymentConfirmUrl = "http://designgallery.git.or.th/paymentconfirm?order=" + orderId;
-    let msg = "Dear, " + userFullName + "." + "<br>" + "<br>" + "Thank you for your order" + "<br>" + "<br>" + "Your order number is " + orderNumber + "." + "<br>" + "Order date: " + orderCreateDate + "." + "<br>" + "<br>"
+    let msg = "เรียน, คุณ" + userFullName + "." + "<br>" + "<br>" + "ขอบคุณสำหรับคำสั่งซื้อ" + "<br>" + "<br>" + "หมายเลขคำสั่งซื้อคือ " + orderNumber + "." + "<br>" + "วันที่สั่งซื้อ: " + orderCreateDate + "." + "<br>" + "<br>"
+    msg += "<br>" + "<br>" + "รูปแบบการชำระเงิน: " + paymentMethodTxt + "." + "<br>" + "ยอดเงิน: ฿" + paymentAmount + "."
+    if (paymentMethod == 1) {
+        msg += "<br>" + "<br>" + "กรุณาโอนเงินชำระค่าสินค้าได้ที่" + "<br>" + "<br>" + "ธนาคารไทยพาณิชย์ สาขาปาโซ่ทาวเวอร์" + "<br>" + "ชื่อบัญชี: สถาบันวิจัยและพัฒนาอัญมีและเครื่องประดับแห่งชาติ (องค์การมหาชน)" + "<br>" + "หมายเลขบัญชี: 245-207440-3"
+        msg += "<br>" + "<br>" + "เมื่อชำระค่าสินค้าเรียบร้อยแล้วกรุณา แจ้งชำระเงินได้ที่" + "<br>" + "<br>" + "<a href='" + paymentConfirmUrl + "'>คลิกเพื่อแจ้งชำระเงินค่าสินค้า</a>"
+    }
+    msg += "<br>" + "<br>" + "ขอบคุณค่ะ," + "<br>" + "GIT Jewely Design Gallery Team <br><br>"
+
+    msg += "Dear, " + userFullName + "." + "<br>" + "<br>" + "Thank you for your order" + "<br>" + "<br>" + "Your order number is " + orderNumber + "." + "<br>" + "Order date: " + orderCreateDate + "." + "<br>" + "<br>"
     msg += "<br>" + "<br>" + "Payment Method: " + paymentMethodTxt + "." + "<br>" + "Payment amount: ฿" + paymentAmount + "."
     if (paymentMethod == 1) {
-        msg += "<br>" + "<br>" + "Please transfer money to the following account." + "<br>" + "<br>" + "Bank: ธนาคารไทยพาณิชย์ สาขาปาโซ่ทาวเวอร์" + "<br>" + "Account Name: สถาบันวิจัยและพัฒนาอัญมีและเครื่องประดับแห่งชาติ (องค์การมหาชน)" + "<br>" + "Account Number: 245-207-440-3"
+        msg += "<br>" + "<br>" + "Please transfer money to the following account." + "<br>" + "<br>" + "Bank: Siam Commercial Bank Public Company Limited" + "<br>" + "Account Name: The Gem and Jewelry Institute of Thailand (Public Organization)" + "<br>" + "Account Number: 245-207440-3"
         msg += "<br>" + "<br>" + "Please send a copy of the payment slip to confirm the payment." + "<br>" + "<br>" + "<a href='" + paymentConfirmUrl + "'>Confirem payment Click Here</a>"
     }
     msg += "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
@@ -146,7 +157,8 @@ const OrderTranfer = async (to, orderNumber, orderCreateDate, paymentSlip, payme
     // paymentAmount = "";
     // let msg = "Payment Confirmation" + "<br>" + "<br>" + "Order number: " + orderNumber + "." + "<br>" + "Order date: " + orderCreateDate + "." + "<br>" + "<br>" + "Payment Slip: " + paymentSlip + "." + "<br>" + "Payment date " + paymentDate + "." + "<br>" + "Payment amount: ฿" + paymentAmount + "." + "<br>" + "<br>" + "Please Check Payment Information and Approve" + "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
 
-    let msg = "Payment Confirmation" + "<br>" + "<br>" + "Order number: " + orderNumber + "." + "<br>" + "Order date: " + orderCreateDate + "." + "<br>" + "<br>" + "Payment date " + paymentDate + "." + "<br>" + "Payment amount: ฿" + paymentAmount + "." + "<br>" + "<br>" + "Please Check Payment Information and Approve" + "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
+    let msg = "แจ้งชำระเงิน" + "<br>" + "<br>" + "หมายเลขคำสั่งซื้อ: " + orderNumber + "." + "<br>" + "วันที่สั่งซื้อ: " + orderCreateDate + "." + "<br>" + "<br>" + "วันที่ชำระเงิน:" + paymentDate + "." + "<br>" + "ยอดเงิน:" + paymentAmount + "บาท" + "<br>" + "<br>" + "กรุราตรวจสอบยอดชำระ และ อนุมัติคำสั่งซื้อ" + "<br>" + "<br>" + "ขอบคุณค่ะ," + "<br>" + "GIT Jewely Design Gallery Team <br><br>"
+        msg += "Payment Confirmation" + "<br>" + "<br>" + "Order number: " + orderNumber + "." + "<br>" + "Order date: " + orderCreateDate + "." + "<br>" + "<br>" + "Payment date: " + paymentDate + "." + "<br>" + "Payment amount: ฿" + paymentAmount + "." + "<br>" + "<br>" + "Please Check Payment Information and Approve" + "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
 
     // console.log(adminEmail)
 
@@ -163,7 +175,8 @@ const ApproveOrderTranfer = async (to, userFullName, orderNumber, orderCreateDat
     // let paymentSlip = "";
     // let paymentDate = "";
     // let paymentAmount = "";
-    let msg = "Dear, " + userFullName + "." + "<br>" + "<br>" + "Your payment has been approved." + "<br>" + "<br>" + "Order number: " + orderNumber + "." + "<br>" + "Order date: " + orderCreateDate + "." + "<br>" + "<br>" + "Payment approved date: " + paymentDate + "." + "<br>" + "<br>" + "Your Can download A Desing in you Order Page" + "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
+    let msg = "เรียน, คุณ" + userFullName + "." + "<br>" + "<br>" + "รายการแจ้งชำระเงินของคุณได้รับการอนุมัติเรียบร้อยแล้ว." + "<br>" + "<br>" + "หมายเลขคำสั่งซื้อ: " + orderNumber + "." + "<br>" + "วันที่สั่งซื้อ: " + orderCreateDate + "." + "<br>" + "<br>" + "วันที่อนุมัติ: " + paymentDate + "." + "<br>" + "<br>" + "คุณสามารถดาวน์โหลดผลงานการออกแบบได้ที่หน้า <b>คำสั่งซื้อ</b>" + "<br>" + "<br>" + "ขอบคุณค่ะ," + "<br>" + "GIT Jewely Design Gallery Team <br><br>"
+        msg += "Dear, " + userFullName + "." + "<br>" + "<br>" + "Your payment has been approved." + "<br>" + "<br>" + "Order number: " + orderNumber + "." + "<br>" + "Order date: " + orderCreateDate + "." + "<br>" + "<br>" + "Payment approved date: " + paymentDate + "." + "<br>" + "<br>" + "Your Can download A Desing in you <b>Order Page</b>" + "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
 
     // console.log(adminEmail)
 
@@ -181,7 +194,8 @@ const ApproveDesignerOrderTranfer = async (to, userFullName, summaryNumber, orde
     // let paymentSlip = "";
     // let paymentDate = "";
     // let paymentAmount = "";
-    let msg = "Dear, " + userFullName + "." + "<br>" + "<br>" + "Your Summary Payment has been approved." + "<br>" + "<br>" + "Summary number: " + summaryNumber + "." + "<br>" + "<br>" + "Summary Rang:" + orderRang + "." + "<br>" + "<br>" + "Payment date: " + paymentDate + "." + "<br>" + "Payment amount: ฿" + paymentAmount + "." + "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
+    let msg = "เรียน, คุณ" + userFullName + "." + "<br>" + "<br>" + "รายการรายได้ของคุณได้รับการชำระเงินเรียบร้อยแล้ว." + "<br>" + "<br>" + "หมายเลข: " + summaryNumber + "." + "<br>" + "<br>" + "ช่วงวันที่คำนวณยอด:" + orderRang + "." + "<br>" + "<br>" + "วันที่ชำระ: " + paymentDate + "." + "<br>" + "ยอดรวมรายรับ: ฿" + paymentAmount + "." + "<br>" + "<br>" + "ขอบคุณค่ะ," + "<br>" + "GIT Jewely Design Gallery Team  <br><br>"
+        msg += "Dear, " + userFullName + "." + "<br>" + "<br>" + "Your Summary Payment has been approved." + "<br>" + "<br>" + "Summary number: " + summaryNumber + "." + "<br>" + "<br>" + "Date Period:" + orderRang + "." + "<br>" + "<br>" + "Payment date: " + paymentDate + "." + "<br>" + "Payment amount: ฿" + paymentAmount + "." + "<br>" + "<br>" + "Thank you," + "<br>" + "GIT Jewely Design Gallery Team"
 
     // console.log(adminEmail)
 
@@ -192,7 +206,8 @@ const ApproveDesignerOrderTranfer = async (to, userFullName, summaryNumber, orde
 const ResetPassword = async (to, newPassword) => {
 
     let subject = "คุณได้ขอเปลี่ยนรหัสผ่าน"
-    let msg = `email คุณได้ขอเปลี่ยนรหัสผ่าน รหัสผ่านใหม่ของคุณคือ ${newPassword}`
+    let msg = `คุณได้ขอเปลี่ยนรหัสผ่าน รหัสผ่านใหม่ของคุณคือ <b>${newPassword} </b>` + "<br> เมื่อคุณเข้าสู่ระบบแล้วกรุณาเปลี่ยนรหัสผ่านใหม่ <br><br>" + "ขอบคุณค่ะ," + "<br>" + "GIT Jewely Design Gallery Team  <br><br>"
+        msg += `You have requested to change your password. Your new password is<b> ${newPassword}</b>` +"<br> Once you have logged in, please change your password. <br><br>" + "ขอบคุณค่ะ," + "<br>" + "GIT Jewely Design Gallery Team  <br><br>"
     // console.log(adminEmail)
 
     SendEmail(subject, msg, to)
